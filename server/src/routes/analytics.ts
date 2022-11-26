2a4f81daf399703adcdc4733ec83c0c1720b8bcd
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2022-11-26 12:58:14 +0100

    feat: Analytics request

--- server/src/routes/analytics.ts
+++ server/src/routes/analytics.ts
@@ -24,17 +24,40 @@ analytics.get('/', async (req, res) => {
             ]
         });
 
-        const completedOrders = allOrders.filter(o => o.status === Status.Done);
-        const newOrders = allOrders.filter(o => o.status === Status.New);
+        const completedOrders = getOrdersByStatus(allOrders, Status.Done);
 
         const completedAmount = completedOrders.length;
         const completedSum = completedOrders.reduce((sum, o) => sum + getOrderCost(o), 0);
+        const averageOrderPrice = completedSum / completedAmount;
 
-        const
+        const canceledAmount = getAmountByStatus(allOrders, Status.Cancel);
+        const canceledPersentage = canceledAmount / allOrders.length * 100;
+
+        const orderPrices = completedOrders.map(o => getOrderCost(o));
 
         res.send({
             completedAmount,
-            completedSum
+            completedSum,
+            canceledAmount,
+            canceledPersentage,
+            averageOrderPrice,
+            activeOrdersAmount: {
+                newOrders: getAmountByStatus(allOrders, Status.New),
+                readyForWork: getAmountByStatus(allOrders, Status.ReadyForWork),
+                inProcess: getAmountByStatus(allOrders, Status.InProcess),
+                resolved: getAmountByStatus(allOrders, Status.Resolved),
+            },
+            ordersAmountPerPeriod: {
+                day: getAmountPerPeriod(allOrders, 24),
+                week: getAmountPerPeriod(allOrders, 24 * 7),
+                month: getAmountPerPeriod(allOrders, 24 * 7 * 30),
+            },
+            ordersAmountInRange: {
+                lessThen50: getOrdersAmountInRange(orderPrices, null, 50),
+                from50to100: getOrdersAmountInRange(orderPrices, 50, 100),
+                from100to200: getOrdersAmountInRange(orderPrices, 100, 200),
+                greaterThen200: getOrdersAmountInRange(orderPrices, 200, null),
+            }
         });
     } catch (err) {
         handleError(res, err);
@@ -45,4 +68,23 @@ function getOrderCost(order: Order): number {
     const workCost = order.works?.reduce((sum, o) => sum + o.cost, 0) || 0;
     const suppliesCost = order.supplies?.reduce((sum, o) => sum + o.pieceCost, 0) || 0;
     return workCost + suppliesCost;
+}
+
+function getOrdersByStatus(allOrders: Order[], status: Status) {
+    return allOrders.filter(o => o.status === status);
+}
+
+function getAmountByStatus(allOrders: Order[], status: Status) {
+    return allOrders.filter(o => o.status === status).length;
+}
+
+function getAmountPerPeriod(orders: Order[], hours: number) {
+    return orders.filter(o => Date.now() - o.createdAt.getTime() < hours*60*1000).length;
+}
+
+function getOrdersAmountInRange(ordersPrices: number[], min: null | number, max: null | number) {
+    return ordersPrices.filter(price => 
+        (min === null || price > min) &&
+        (max === null || price < max) 
+    ).length;
 }
